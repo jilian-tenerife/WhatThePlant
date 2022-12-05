756f91a50c79c2abfe8ee9c0c5574f9971871dc6
Jillian <jillian.tenerife@usjr.edu.ph>
2022-12-05 13:00:59 +0800

    camera screen and top nav

---
+++ whattheplant/lib/screens/camera_screen.dart
@@ -0,0 +1,54 @@
+import 'package:whattheplant/widgets/top_nav.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen(
+      {Key? key, required this.cameraController, required this.initCamera})
+      : super(key: key);
+  final CameraController? cameraController;
+  final Future<void> Function({required bool frontCamera}) initCamera;
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  bool _isFrontCamera = true;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        (widget.cameraController == null)
+            ? Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12)),
+              )
+            : GestureDetector(
+                onDoubleTap: () {
+                  _isFrontCamera = !_isFrontCamera;
+                  widget.initCamera(frontCamera: _isFrontCamera);
+                },
+                child: CameraPreview(widget.cameraController!),
+              ),
+        TopBar(),
+        Positioned(
+            bottom: 95,
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/history.png',
+                  scale: 1.5,
+                ),
+                Image.asset('assets/snap.png'),
+                Image.asset(
+                  'assets/collections.png',
+                  scale: 1.3,
+                )
+              ],
+            ))
+      ],
+    );
+  }
+}
